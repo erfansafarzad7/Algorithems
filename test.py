846415febf7e3e15984e74c1788772793a78002c
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-07-01 19:15:16 +0430

    CaesarCipher

--- test.py
+++ test.py
@@ -4,29 +4,75 @@ Top one
 """
 
 
-def top(lst):
-    values = {}
-    result = []
-    f_val = 0
+# def top(lst):
+#     values = {}
+#     result = []
+#     f_val = 0
+#
+#     for i in lst:
+#         if i in values:
+#             values[i] += 1
+#         else:
+#             values[i] = 1
+#
+#     f_val = max(values.values())
+#
+#     for i in values.keys():
+#         if values[i] == f_val:
+#             result.append(i)
+#         else:
+#             continue
+#
+#     return f"most repeated items:{result} \n values: {values}"
+#
+#
+# print(top([1, 2, 3, 2, 3, 1, 2, 2, 3, 4, 5, 5, 6]))
 
-    for i in lst:
-        if i in values:
-            values[i] += 1
+# -----------------------------------------------------------------
+
+from string import ascii_letters
+
+"""
+Caesar Cipher
+"""
+
+
+def encrypt(string: str, key: int):
+    alpha = ascii_letters
+    result = ''
+
+    for ch in string:
+        if ch not in alpha:
+            result += ch
         else:
-            values[i] = 1
+            new_key = (alpha.index(ch) + key) % len(alpha)
+            result += alpha[new_key]
 
-    f_val = max(values.values())
-
-    for i in values.keys():
-        if values[i] == f_val:
-            result.append(i)
-        else:
-            continue
-
-    return f"most repeated items:{result} \n values: {values}"
+    return result
 
 
-print(top([1, 2, 3, 2, 3, 1, 2, 2, 3, 4, 5, 5, 6]))
+def decrypt(string: str, key: int):
+    key *= -1
+    return encrypt(string, key)
+
+
+def brute_force(string):
+    alpha = ascii_letters
+    key = 1
+    result = ''
+    brute_force_data = {}
+
+    while key <= len(alpha):
+        result = decrypt(string, key)
+        brute_force_data[key] = result
+        result = ''
+        key += 1
+    return brute_force_data
+
+
+print(encrypt('erfan', 7))
+print(decrypt('lymhu', 7))
+print(brute_force('lymhu'))
 
 # -----------------------------------------------------------------
 
