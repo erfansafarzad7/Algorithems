12a6f3af602af8f46a4c185ec1662cc16e8e468e
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-07-01 20:42:03 +0430

    IsIsomorphic

--- test.py
+++ test.py
@@ -101,3 +101,34 @@ Search Insert
 
 # -----------------------------------------------------------------
 
+"""
+Is Isomorphic
+    foo, bar => False
+    foo, bee => True
+    paper, title => True
+"""
+
+
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
+
+    dic = {}
+    set_values = set()
+
+    for i in range(len(s)):
+        if s[i] not in dic:
+            if t[i] in set_values:
+                return False
+            dic[s[i]] = t[i]
+            set_values.add(t[i])
+        else:
+            if dic[s[i]] != t[i]:
+                return False
+
+    return True
+
+
+print(is_isomorphic('paper', 'title'))
+
+# -----------------------------------------------------------------
