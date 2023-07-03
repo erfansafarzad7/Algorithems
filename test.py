b42d4742e94b1ec2ebc436312e1b835823109a8a
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-07-03 11:39:24 +0430

    LastOccurrence

--- test.py
+++ test.py
@@ -399,23 +399,46 @@ First Occurrence
 """
 
 
-def first_occurrence(array, element):
+# def first_occurrence(array, element):
+#     low, high = 0, len(array) - 1
+#
+#     while low <= high:
+#         mid = (low + high) // 2
+#
+#         if low == high:
+#             break
+#         elif array[mid] < element:
+#             low = mid + 1
+#         else:
+#             high = mid
+#
+#     if array[low] == element:
+#         return low
+#
+#
+# print(first_occurrence([2, 3, 3, 4, 4, 4, 5, 5, 6], 5))
+
+# -----------------------------------------------------------------
+
+"""
+Last Occurrence
+    [2, 3, 3, 4, 4, 4, 5, 5, 6], 5 => 7
+"""
+
+
+def last_occurrence(array, element):
     low, high = 0, len(array) - 1
-
     while low <= high:
-        mid = (low + high) // 2
-
-        if low == high:
-            break
-        elif array[mid] < element:
+        mid = (high + low) // 2
+        if (array[mid] == element and mid == len(array) - 1) or \
+                (array[mid] == element and array[mid + 1] > element):
+            return mid
+        elif array[mid] <= element:
             low = mid + 1
         else:
-            high = mid
-
-    if array[low] == element:
-        return low
+            high = mid - 1
 
 
-print(first_occurrence([2, 3, 3, 4, 4, 4, 5, 5, 6], 5))
+print(last_occurrence([2, 3, 3, 4, 4, 4, 5, 5, 6], 5))
 
 # -----------------------------------------------------------------
